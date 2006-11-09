3e7c0e66d636b127a98036ae2c9a503452178c81
Sebastien Bacher <seb128@canonical.com>
2006-11-09 09:52:49 -0800

    Import upstream version 2.17.2.1

---
+++ src/lsof.h
@@ -0,0 +1,13 @@
+#ifndef H_PROCMAN_LSOF_1161179202
+#define H_PROCMAN_LSOF_1161179202
+
+#include <glib/gmacros.h>
+#include "procman.h"
+
+G_BEGIN_DECLS
+
+void procman_lsof(ProcData *data);
+
+G_END_DECLS
+
+#endif /* H_PROCMAN_LSOF_1161179202 */
